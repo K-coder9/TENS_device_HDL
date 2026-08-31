cfbdd40391e6bd98c53f04382250ccd9f7ad1511
K-coder9 <156027809+K-coder9@users.noreply.github.com>
2026-08-31 11:47:59 +0100

    Update test.py
    Change uio_i values to attainable 0 or zero and get rid of uo_out = 50 since this is unachievablt only the last bit changes

--- test/test.py
+++ test/test.py
@@ -18,23 +18,24 @@ async def test_project(dut):
     dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
-    dut.uio_in.value = 0
+    dut.uio_in.value = 1 #use the fast clk first 
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
-
+    #add in clk cycles before testing slow speed 
+    await ClockCycles(dut.clk,10)
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
     dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    dut.uio_in.value = 0 #use the low speed
 
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+   # assert dut.uo_out.value == 50 #only one bit changes so this is never reached 
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
