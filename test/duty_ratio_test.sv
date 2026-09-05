f954fe0e093f11bf5fdeb177e8a84dcb0db3c944
K-coder9 <156027809+K-coder9@users.noreply.github.com>
2026-09-05 16:07:29 +0100

    Update duty_ratio_test.sv
    Fix comments and add duration test for the burst mode

--- test/duty_ratio_test.sv
+++ test/duty_ratio_test.sv
@@ -7,9 +7,12 @@ module tens_pwm_test_tb;
   logic ena = 1'b1;
   logic [7:0] ui_in;
   logic [7:0] uio_in;
-  
-  //wire all the probable signals 
+
   wire [7:0] uo_out,uio_out,uio_oe;
+  
+  //variables for timing test  
+  realtime burst_end;
+  realtime begin_burst,duration;
   
   tt_um_K_coder_9 dut(.ui_in(ui_in),.uo_out(uo_out),.uio_in(uio_in),.uio_out(uio_out),.uio_oe(uio_oe),.ena(ena),.rst_n(rst_n),.clk(clk));
   
@@ -19,11 +22,16 @@ module tens_pwm_test_tb;
    //display time in ms 
   logic [31:0] seed;
   static logic [7:0] pwm_out_count = 8'b0;
+  
   rst_n = 1'b0;
   @(posedge clk);
   @(posedge clk);
   rst_n = 1'b1;
   
+   //---------------------------------------------------------//
+   //                    Duty Ratio Test                      //
+   //---------------------------------------------------------//
+   
   //set a random value for dp and check the fast mode
    uio_in[0] = 1'b1;
    uio_in[1] = 1'b0;
@@ -31,13 +39,9 @@ module tens_pwm_test_tb;
     for(int i=0;i<10;i+=1)begin 
       seed = 2;
       ui_in = $random(seed);
-      
-      $dumpfile("tb.vcd");
-      $dumpvars(0,tens_pwm_test_tb);
     end
     
-    // count the output of the duty cycle?
-    //is the ratio out of 256 right to the input dp pins 
+    // count the output of the duty cycle to check if the ratio out of 256 matches the input dp pins 
     
     // count the clock periods uo_out is high for:
     repeat(256)begin 
@@ -59,22 +63,33 @@ module tens_pwm_test_tb;
       end else if((pwm_out_count != ui_in)&&!uio_in[0]&&!uio_in[1])begin
         $display("TEST has failed for low speed. DP entered was %b but the measured output dp was: %b",ui_in,pwm_out_count);
       end
-    
-   //add in burst mode test sample if the first pulse and last pulse are 100ms a part 
-   //take time of the burst_en goes down to zero it is normally on to allow continuous so measure what time the burst is off , epxpect to be around 200 ms
+   //------------------------------------------------------------------------------//
+   //                                Burst Mode Test                               //
+   //---------------------------------------------------------------------------- -//
+   
+   //if divider correctly creats 100Hz enable the burst should be on for 200 ms
+   
    uio_in[1] = 1'b1;
    if(uio_in[1])begin
-   realtime burst_end;
-   @(negedge dut.u_burst_gen.burst_en);
-   burst_end = $realtime;
-   
-   $display("Burst finished at time: %0t",burst_end);
+     @(posedge dut.u_burst_gen.burst_en);
+     begin_burst = $realtime;
+     $dumpfile("tb.vcd");
+     $dumpvars(0,tens_pwm_test_tb);
+     
+     @(negedge dut.u_burst_gen.burst_en);
+   	 burst_end = $realtime;
+     duration = burst_end - begin_burst;
+     $display("Burst duration lasted: %0t",duration);
+     
+     if(duration >190_000_000.0 && duration <210_000_000.0)//scaled down comparison to nanoseconds
+       $display("The burst test passed");
+     else 
+       $display("The burst test has failed");
    end else begin
      $display("There was not burst test");
    end
    $finish;
   end 
-  
-  //set a random value for dp and check the slow mode 
+ 
   
 endmodule
